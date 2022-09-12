91087f0fd870a5a8cac0fe657a93367a2cc96955
thedownface <83498562+thedownface@users.noreply.github.com>
2022-09-12 23:58:00 +0530

    Create identicaltrees.cpp

---
+++ identicaltrees.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+    public:
+    //Function to check if two trees are identical.
+    bool isIdentical(Node *r1, Node *r2)
+    {
+        if(r1 == NULL && r2 == NULL){
+            return true;
+        }
+        else if(r1 == NULL && r2 !=NULL){
+            return false;
+        }
+        else if(r2 == NULL && r1 != NULL){
+            return false;
+        }
+        bool left = isIdentical(r1->left,r2->left);
+        bool right = isIdentical(r1->right,r2->right);
+        bool curr_val = (r1->data)== (r2->data);
+        
+        if(left & right & curr_val){
+            return true;
+        }
+        else return false;
+    }
+};
